b5371d67f5b96b429acafda6e555952a4a76c55a
VikramAditya <deepkumarkhamaru@gmail.com>
2023-07-26 12:35:41 +0530

    blank canvas

---
+++ src/Components/Display.tsx
@@ -0,0 +1,9 @@
+const Display = () => {
+  return (
+    <div className="w-full h-[500vh] min-h-[500vh] flex flex-col justify-between items-center">
+      <div></div>
+    </div>
+  );
+}
+
+export default Display;
